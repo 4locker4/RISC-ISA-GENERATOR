6617cff383f291725c59be3c37709101c1765809
4Locker4 <e.remchukov2006@gmail.com>
2026-01-22 16:28:02 +0300

    base

---
+++ Target/RISC-V/32I.rb
@@ -0,0 +1,18 @@
+require_relative "encoding"
+require_relative "regfile"
+require_relative "../../Generic/base"
+
+module RV32I
+    extend SimInfra
+    Instruction(:ADD, XReg(:rd), XReg(:rs1), XReg(:rs2)) {
+        encoding *format_r_alu(:add, rd, rs1, rs2)
+        asm { "ADD #{rd}, #{rs1}, #{rs2}" }
+        code { rd[]= rs1 + rs2 }
+    }
+
+    Instruction(:SUB, XReg(:rd), XReg(:rs1), XReg(:rs2)) {
+        encoding *format_r_alu(:sub, rd, rs1, rs2)
+        asm { "SUB #{rd}, #{rs1}, #{rs2}" }
+        code { rd[]= rs1 - rs2 }
+    }
+end
---
+++ main.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+require_relative "Generic/decoder"
+require_relative "Generic/base"
+require_relative "Generic/builder"
+require_relative "Target/RISC-V/32I.rb"
+
+SimInfra.serialize
